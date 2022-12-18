2c0e629168c8b69c06b4fcea08f0a88cd8466282
ProstoKylinar <102613624+ProstoKylinar@users.noreply.github.com>
2022-12-18 22:40:58 +0300

    Add files via upload

---
+++ functions.hpp
@@ -0,0 +1,18 @@
+#ifndef LABA8_FUNCTIONS_HPP
+#define LABA8_FUNCTIONS_HPP
+
+#include <iostream>
+
+long long gcd(long long a, long long b);
+
+long long mul(long long a, long long b, long long m);
+
+long long pows(long long a, long long b, long long m);
+
+bool ferma(long long x);
+
+bool isinMagicTriple(long long x, long long n);
+
+long long SumProstNumber(long long n);
+
+#endif //LABA8_FUNCTIONS_HPP
